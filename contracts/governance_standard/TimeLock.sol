82c58d914ad7a18c627ecc114002fff1a79f3f8a
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-02-07 12:28:46 +0500

    chore: add comment explaining admin

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -7,6 +7,7 @@ contract TimeLock is TimelockController {
     // minDelay is how long you have to wait before executing
     // proposers is the list of addresses that can propose
     // executors is the list of addresses that can execute
+    // admin is optional account to be granted admin role; disable with zero address
     constructor(
         uint256 minDelay,
         address[] memory proposers,
